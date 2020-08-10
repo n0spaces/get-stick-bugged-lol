10273f85f039c048e558b407bbfb0359dce91a7b
n0spaces <7495188+n0spaces@users.noreply.github.com>
2020-08-10 17:46:43 -0500

    Move most of the code into one file

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import sys
+
 from PIL import Image
 
-import line_interp
-import segment_finder
-import video_generator
+from generator import find_segments, interp_segments, generate_video
 
 im = Image.open(sys.argv[1])
-num_frames = 52  # video will be 30 fps
 
-segments = segment_finder.find_segments(im)
+segments = find_segments(im)
 
-segment_frames = line_interp.interp_segments(segments, num_frames)
+segment_frames = interp_segments(segments)
 
 if len(sys.argv) > 2:
-    video_generator.generate_video(segment_frames, im, filename=sys.argv[2])
+    generate_video(segment_frames, im, filename=sys.argv[2])
 else:
-    video_generator.generate_video(segment_frames, im)
+    generate_video(segment_frames, im)
