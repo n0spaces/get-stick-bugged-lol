7df25c3676d708eb971ecb66029736e29e75146c
n0spaces <7495188+n0spaces@users.noreply.github.com>
2020-08-12 19:55:40 -0500

    Fix installing and executing module

--- gsbl/__main__.py
+++ gsbl/__main__.py
@@ -2,12 +2,13 @@ def main():
     import argparse
     import gsbl
 
-    parser = argparse.ArgumentParser(description="Create a 'get stick bugged lol' video from an image.")
+    parser = argparse.ArgumentParser(prog='gsbl', description="Create a 'get stick bugged lol' video from an image.")
     parser.add_argument('input', help='the image file to be used to generate the video (png, jpg, ...)')
     parser.add_argument('output', help='the video file to be generated and saved (mp4, webm, ...)')
     parser.add_argument('--line-color', dest='line_color', nargs=3, type=int, default=[255, 255, 211],
                         metavar=('R', 'G', 'B'), help='RGB color to use for line segments (default: 255 255 211)')
-    parser.add_argument('--bg-color', dest='bg_color', nargs=3, type=int, default=[125, 115, 119], metavar=('R', 'G', 'B'),
+    parser.add_argument('--bg-color', dest='bg_color', nargs=3, type=int, default=[125, 115, 119],
+                        metavar=('R', 'G', 'B'),
                         help='RGB color to use for background after image disappears (default: 125 115 119)')
 
     args = parser.parse_args()
--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 import setuptools
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='get-stick-bugged-lol',
     version='0.0.1',
     author='n0spaces',
     description="'Get stick bugged' video generator",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/n0spaces/get_stick_bugged_lol',
     packages=setuptools.find_packages(),
     package_data={'gsbl': ['media/*.*']},
-    entry_points={'console_scripts': ['gsbl=gsbl.main_cli:main']},
+    entry_points={'console_scripts': ['gsbl=gsbl.__main__:main']},
+    install_requires=['pylsd-nova', 'numpy', 'Pillow', 'moviepy'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
